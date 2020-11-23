69ebacca5ffe6885ebe92515957613d4f9fa048e
周玉娟 <1715061643@qq.com>
2020-11-23 08:46:48 +0800

    第十次课/js初步学习.js 第十次课/复习块级样式.html 第十次课/复习行内样式.html 第十次课/左侧tab导航栏.html 第十一次课/CSS过渡.html 第十一次课/换样式/index.htm 第十一次课/换样式/index1.css 第十一次课/换样式/index2.css 第十一次课/浮动.html

---
+++ 第十次课/js初步学习.js
@@ -0,0 +1,10 @@
+
+function orange(){
+var array = [123,45,67,89,66,88,99]
+for (let index = 0;index < array.length; index++){
+    const element = array[index];
+    console.log(element);
+}
+
+}
+orange();
